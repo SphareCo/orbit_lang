10e10e580d5a77f0be91cc539e544a57e4c5333d
adarshsharma1350 <211204748+coodig@users.noreply.github.com>
2025-11-30 10:35:30 +0530

    specific node (literal expression)
    Here we define exactly how each feature of Orbit is stored.
    
    Stores raw values like numbers or strings.

---
+++ include/AST.h
@@ -0,0 +1,33 @@
+#ifndef AST_H
+#define AST_H
+
+#include <string>
+#include <memory>
+#include <vector>
+
+#include <iostream>
+
+#include "TokenType.h"
+
+using namespace std;
+
+struct ASTNode
+{
+    virtual ~ASTNode() = default;
+};
+
+struct Statement : public ASTNode
+{
+};
+struct Expression : public ASTNode
+{
+};
+
+struct LiteralExpr : public Expression
+{
+    string value;
+    TokenType type;
+
+    LiteralExpr(string val, TokenType t) : value(val), type(t) {};
+};
+#endif
