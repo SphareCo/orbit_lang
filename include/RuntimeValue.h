b396cd086eb4b479528be5f5b4daf7e5b2a9732c
adarshsharma1350 <211204748+coodig@users.noreply.github.com>
2025-12-04 16:23:24 +0530

    feat(interpreter): Add RuntimeValue system to handle dynamic data types
    Introduced the `RuntimeValue` architecture to solve C++ static typing limitations for the Orbit Interpreter.
    
    **Key Changes in `include/RuntimeValue.h`:**
    - **Poly-value Container:** Created a struct capable of holding `Number` (double), `String`, `Boolean`, and `Null` values dynamically.
    - **Type Tagging:** Implemented `ValueType` enum to identify the active data type at runtime.
    - **Hinglish Output Support:** Added `asString()` helper method that automatically formats outputs (e.g., converting `true` -> "sahi", `false` -> "galat").
    - **Smart Formatting:** Included logic to strip trailing zeros from floating-point numbers for cleaner display.
    
    This update lays the foundation for the upcoming Interpreter (v0.3) to evaluate AST nodes and perform calculations.

---
+++ include/RuntimeValue.h
@@ -0,0 +1,83 @@
+// This is header to insure that a file include only once
+#ifndef RUNTIME_VALUE_H
+
+#define RUNTIME_VALUE_H
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+enum ValueType
+{
+    VAL_NULL,
+    VAL_NUMBER,
+    VAL_STRING,
+    VAL_BOOL
+};
+
+struct RuntimeValue
+{
+    ValueType type;
+
+    double numberVal;
+    string stringVal;
+    bool boolVal;
+
+    RuntimeValue()
+    {
+        type = VAL_NULL;
+        numberVal = 0;
+        boolVal = false;
+    }
+
+    RuntimeValue(double val)
+    {
+        type = VAL_NUMBER;
+        numberVal = val;
+        boolVal = false;
+    }
+
+    RuntimeValue(string val)
+    {
+        type = VAL_STRING;
+        stringVal = val;
+        numberVal = 0;
+        boolVal = false;
+    }
+
+    RuntimeValue(bool val)
+    {
+        type = VAL_BOOL;
+        boolVal = val;
+        numberVal = 0;
+    }
+
+    string asString() const
+    {
+        if (type == VAL_NUMBER)
+        {
+            string s = to_string(numberVal);
+            s.erase(s.find_last_not_of('0') + 1, string::npos);
+            if (s.back() == '.')
+                s.pop_back();
+            return s;
+        }
+
+        else if (type == VAL_STRING)
+        {
+            return stringVal;
+        }
+        else if (type == VAL_BOOL)
+        {
+            return boolVal ? "ture" : "false";
+        }
+        else
+        {
+            return "empty";
+        }
+    }
+};
+
+#endif
