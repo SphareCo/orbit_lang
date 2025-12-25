6d09d050519661ec2d0edfbf1acfe93c2653ae06
adarshsharma1350 <211204748+coodig@users.noreply.github.com>
2025-12-25 12:38:52 +0530

    AST.h file completed
    AST file have visitor pattern so that Interpreter visits the nodes easily...

--- include/AST.h
+++ include/AST.h
@@ -10,14 +10,24 @@
 using namespace std;
 
 // It's a forward declearation to tell the compiler that visitor is coming now
-struct visitor;
+struct Visitor;
+
+class Visitor
+{
+public:
+    virtual void visit(LiteralExpr *expr) = 0;
+    virtual void visit(VariableExpr *expr) = 0;
+    virtual void visit(BinaryExpr *expr) = 0;
+    virtual void visit(PrintStatement *expr) = 0;
+    virtual void visit(VarDeclaration *expr) = 0;
+};
 // Abstract Syntax Tree node = Base node
 struct ASTNode
 {
 
     virtual ~ASTNode() = default;
 
-    virtual void accept(visitor &v) = 0;
+    virtual void accept(Visitor &v) = 0;
 };
 
 // These are the categories
@@ -34,12 +44,14 @@ struct LiteralExpr : public Expression
     TokenType type;
 
     LiteralExpr(string val, TokenType t) : value(val), type(t) {}
+    void accept(Visitor &v) override { v.visit(this); }
 };
 
 struct VariableExpr : public Expression
 {
     string name;
     VariableExpr(string n) : name(n) {}
+    void accept(Visitor &v) override { v.visit(this); }
 };
 
 struct BinaryExpr : public Expression
@@ -49,6 +61,8 @@ struct BinaryExpr : public Expression
     unique_ptr<Expression> right;
 
     BinaryExpr(unique_ptr<Expression> l, TokenType o, unique_ptr<Expression> r) : left(move(l)), op(o), right(move(r)) {}
+
+    void accept(Visitor &v) override { v.visit(this); }
 };
 
 struct PrintStatement : public Statement
@@ -56,6 +70,7 @@ struct PrintStatement : public Statement
     unique_ptr<Expression> expression;
 
     PrintStatement(unique_ptr<Expression> expr) : expression(move(expr)) {}
+    void accept(Visitor &v) override { v.visit(this); }
 };
 
 struct VarDeclaration : public Statement
@@ -64,6 +79,7 @@ struct VarDeclaration : public Statement
     unique_ptr<Expression> initializer;
 
     VarDeclaration(string n, unique_ptr<Expression> init) : name(n), initializer(move(init)) {}
+    void accept(Visitor &v) override { v.visit(this); }
 };
 
 #endif
